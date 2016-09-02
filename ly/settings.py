68a81614c01d8d04e2b93ec29a31aca435bb5750
Ronny Wang <ronnywang@gmail.com>
2016-09-02 15:37:18 +0800

    change debug to env

--- ly/settings.py
+++ ly/settings.py
@@ -3,7 +3,7 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True'),
 
 ADMINS = (
     ('thewayiam', 'twly.tw@gmail.com'),
